b6416b663e29c6d65d4d5b578f32507870a9f7b0
development0261 <pythondev65@gmail.com>
2021-12-24 11:38:03 +0530

    package version

--- setup.py
+++ setup.py
@@ -19,5 +19,5 @@ setup(
   classifiers=classifiers,
   keywords='sharepoint', 
   packages=find_packages(),
-  install_requires=['setuptools','pandas','pandasql==0.7.3','Office365-REST-Python-Client','shillelagh==1.0.5']
+  install_requires=['setuptools','pandas','pandasql==0.7.3','Office365-REST-Python-Client','shillelagh==1.0.5', 'apsw_wheels-3.36.0.post1-cp38-cp38-win_amd64.whl']
 )
